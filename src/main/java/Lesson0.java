5ec98d717fb11ceec862187766ed0d34e6d71341
Shkulepa-Mark <skulepamark@gmail.com>
2024-02-13 16:19:49 +0300

    Lesson0

---
+++ src/main/java/Lesson0.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Lesson0 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String user = scanner.next();
+
+        System.out.println("Hello " + user);
+    }
+}
