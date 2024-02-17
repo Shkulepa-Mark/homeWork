6165db2af0009a4af8a68aa85229464b097dcd4c
Shkulepa-Mark <skulepamark@gmail.com>
2024-02-17 14:21:13 +0300

    add calculator

---
+++ src/main/java/Lesson2/Calculator.java
@@ -0,0 +1,57 @@
+package Lesson2;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public void calculate(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите два числа: ");
+        double operand1 = scanner.nextDouble();
+        double operand2 = scanner.nextDouble();
+
+        System.out.println("Выберите операцию: ");
+        System.out.println("1. Сумма");
+        System.out.println("2. Разность");
+        System.out.println("3. Умножение");
+        System.out.println("4. Деление");
+
+        int sign = scanner.nextInt();
+
+        switch (sign)
+        {
+            case 1:
+            {
+                System.out.println("Сумма " + operand1 + " и " + operand2 + " равна: " + (operand1+operand2));
+                break;
+            }
+            case 2:
+            {
+                System.out.println("Разность " + operand1 + " и " + operand2 + " равна: " + (operand1-operand2));
+                break;
+            }
+            case 3:
+            {
+                System.out.println("Произведение " + operand1 + " и " + operand2 + " равна: " + (operand1*operand2));
+                break;
+            }
+            case 4:
+            {
+                if(operand2 == 0)
+                {
+                    System.out.println("Ошибка! На ноль делить нельзя!");
+                    break;
+                }
+                else
+                {
+                    System.out.println("Частное " + operand1 + " и " + operand2 + " равна: " + (operand1/operand2));
+                }
+                break;
+            }
+            default:
+            {
+                System.out.println("Такого пункта в меню калькулятора нет");
+                break;
+            }
+        }
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,29 @@
 import Lesson0.HelloUser;
+import Lesson2.Calculator;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        HelloUser helloUser = new HelloUser();
-        helloUser.userName();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Выберите номер задачи");
+        int select = scanner.nextInt();
+
+        switch (select)
+        {
+            case 1:
+            {
+                HelloUser helloUser = new HelloUser();//0
+                helloUser.userName();
+                break;
+            }
+            case 2:
+            {
+                Calculator calculator = new Calculator();
+                calculator.calculate();//1
+                break;
+            }
+        }
     }
 }
