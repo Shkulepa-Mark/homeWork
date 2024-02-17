78d157f696ff11aa1eb0aa6ce112325ba555631d
Shkulepa-Mark <skulepamark@gmail.com>
2024-02-17 13:37:42 +0300

    printHelloUser

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import Lesson0.HelloUser;
+
+public class Main {
+    public static void main(String[] args) {
+        HelloUser helloUser = new HelloUser();
+        helloUser.userName();
+    }
+}
